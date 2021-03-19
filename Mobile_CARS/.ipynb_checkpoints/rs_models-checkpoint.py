8864db3b0690f77cd4a87201194ea5dc81cf1db7
Lorenzo <lorenzo.dalessandro1@studenti.unimi.it>
2021-03-19 13:45:33 +0100

    working on train and gridsearch for MF

--- Mobile_CARS/.ipynb_checkpoints/rs_models-checkpoint.py
+++ Mobile_CARS/.ipynb_checkpoints/rs_models-checkpoint.py
@@ -3,6 +3,13 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Input, Embedding, Flatten, Concatenate, Lambda
 from keras.optimizers import Adam
+from scipy.sparse import coo_matrix, csr_matrix
+from implicit.evaluation import AUC_at_k, precision_at_k, train_test_split
+from implicit.als import AlternatingLeastSquares
+import numpy as np
+from sklearn.model_selection import KFold
+from tqdm import tqdm
+
 
 def MF(param):
     # Input variables
@@ -113,3 +120,160 @@ def mobile_model(neurons, layers, learn_rate):
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer=Adam(lr=learn_rate))
     return model
+
+
+def kfold_split(df, x, y, n_splits=5):
+    """
+    Generator that split a dataframe in train and test. Every yield returns a new split until n_splits iterations are done
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+        Dataframe to split
+    x : list of strings
+        x labels
+    y: list of strings
+        y labels
+    n_splits : int
+        how many splits the generator can yield
+        
+    Returns
+    -------
+    (x_train, y_train, x_test, y_test) : panda dataframes
+        four pandas dataframe for train and test
+    """
+    kf = KFold(n_splits=n_splits, random_state=42, shuffle=True)
+
+    for train_index, test_index in kf.split(df[x], df[y]):
+        x_train, x_test = df[x].loc[train_index, :], df[x].loc[test_index, :]
+        y_train, y_test = df[y].loc[train_index], df[y].loc[test_index]
+        yield x_train, y_train, x_test, y_test
+
+
+def kfold_train(model, param, df, context_labels=[], n_splits=2):
+    """
+    Train a model with two or more input using Kfold
+    
+    Parameters
+    ----------
+    model : function
+        A function that return a compiled keras model
+    param : dictionary
+        A dictionary that contains model parameters like learning rate, epochs, batch size
+    df : pandas dataframe
+        Data on which the model will be trained
+        
+    Returns
+    -------
+    results : numpy array
+        An array containing metrics averages obtained by evaluating the model on n_splits
+    """
+    x_labels = ['user', 'item'] 
+    y_labels = 'rating'
+    df = df.sample(frac=1) # shuffle dataset
+    kfold = kfold_split(df, x_labels+context_labels, y_labels, n_splits) # generator that returns training and test index
+    idx = 0
+
+    for x_train, y_train, x_test, y_test in kfold:
+        net = model(param)
+
+        input_list = [x_train[e] for e in x_labels] # split user, item input
+        input_list = [input_list + [x_train[context_labels]] if context_labels else input_list] # add context if it's available
+        net.fit(input_list, y_train, epochs=param['epochs'], batch_size=param['batch_size'], verbose=False)
+
+        input_list = [x_test[e] for e in x_labels] # same split for test values
+        input_list = [input_list + [x_test[context_labels]] if context_labels else input_list]
+        if idx == 0: # if it is the first fold, create results array
+            results = np.array(net.evaluate(input_list, y_test, batch_size=512, verbose=False))
+        else: # else add new results to array
+            results = np.add(results, net.evaluate(input_list, y_test, batch_size=512, verbose=False))
+        idx = idx + 1
+    return results/idx
+
+
+def train_mf(df, factors=128, regularization=5, iterations=50, n_splits=10, k=10):
+    """
+    Train ALS matrix factorization model from implicit library on n splits
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+        user, item, rating data
+    factors: int
+        The number of latent factors to compute
+    regularization : float
+        The regularization factor to use
+    iterations : int
+        The number of ALS iterations to use when fitting data
+    n_splits: int
+        the number of train/test
+        
+    Returns
+    -------
+    results : numpy array
+        An array containing metrics averages obtained by evaluating the model on n_splits
+    """
+    ratings = coo_matrix((df['rating'].astype(np.float32),
+                         (df['item'],
+                          df['user']))).tocsr()
+    results = np.zeros(2)
+    for x in range(n_splits):
+        train, test = train_test_split(ratings, train_percentage=0.8)
+        model = AlternatingLeastSquares(factors=factors, regularization=regularization, iterations=iterations, calculate_training_loss=True)
+        model.fit(train, show_progress=False)
+
+        auc = AUC_at_k(model, train.T.tocsr(), test.T.tocsr(), K=k, show_progress=False, num_threads=4)
+        precision = precision_at_k(model, train.T.tocsr(), test.T.tocsr(), K=k, show_progress=False, num_threads=4)
+        results = np.add(results, [auc, precision])
+    return results/(x+1)
+
+
+
+def mf_grid_search(df, factors, regularization, iterations, n_splits, monitor, k):
+    """
+    Grid search for ALS matrix factorization from implicit library
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+        user, item, rating data
+    factors: list of int
+        The number of latent factors to compute
+    regularization : list of float
+        The regularization factor to use
+    iterations : list of int
+        The number of ALS iterations to use when fitting data
+    n_splits: int
+        the number of train/test
+    monitor: string
+        Metric that is used to select the best parameters combination
+        
+    Returns
+    -------
+    best_parameters : numpy array
+        An array containing the best parameters combination (factors, regularization, iterations)
+    """
+    combinations = []  # possible combination of grid search parameters   
+    for x in factors:  # fill combinations list         
+        for y in regularization: 
+            for z in iterations:
+                combinations.append((x, y, z)) 
+             
+     # select which metric is used to select the best parameters combination
+    if monitor == 'auc':
+        idx = 0
+    elif monitor == 'precision':
+        idx = 1
+    else:
+        raise Exception("Unknown metric, possible metrics are: auc, precision")
+
+
+    best_metric = 0
+    best_parameters = []
+    for factors, regularization, iterations in tqdm(combinations):
+        results = train_mf(df, factors=factors, regularization=regularization, 
+                           iterations=iterations, n_splits=n_splits, k=k) # train model on n splits
+        if results[idx] > best_metric:
+            best_parameters = [factors, regularization, iterations]
+            best_metric = results[idx]
+    return best_parameters
